5f39b46e86d8d284217ed27ba362e9396420cfc7
suixian.gong <suixian.gong@sumscope.com>
2021-06-10 11:47:52 +0800

    prod

--- pack_svc_C.sh
+++ pack_svc_C.sh
@@ -16,7 +16,7 @@ pack_app(){
 #  echo "打印安装"
   npm i
 #  echo " 编译"
-  npm run build --prod
+  npm run build -- --prod
 
 #  mkdir -p  $appName/
 #  ls
@@ -27,23 +27,5 @@ pack_app(){
   tar -czvf "$appName.tar.gz" $appName/
 #  set +e
 }
-pack_third(){
-  thirdDir=$1
-  appName=$2
-  packageName=$3
-  version=$4
-  name=$packageName-3rdparty-centos7-$version
 
-  echo "Packaging $thirdDir"
-  cd -
-  rm -rf $appName
-  mkdir $appName
-  cp -a $thirdDir/*/*.so* $appName/
-  cp -a $thirdDir/log4cxx/log4cxx.properties $appName/
-
-  set -e
-  cd "$appName"
-  tar -czvf ../"$name.tar.gz" *
-  set +e
-}
 pack_app "$srcDir" "$appName" "$packageName" "$version"
